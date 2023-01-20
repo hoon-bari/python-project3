1337f1ba13a6a4982b0fe45e587bfc9fd320d2d2
SeungHoonChoi <cshoon036@naver.com>
2023-01-20 11:43:09 +0900

    진짜 찐으로 다했다(워드클라우드에 크롤링까지)

--- mynews_site/settings.py
+++ mynews_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-h_@6wa6-zy$k@%2qjzxl8a!gwr6rwrikbvj2_h8wone6()kk1f"
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['52.79.84.85']
